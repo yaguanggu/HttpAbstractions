c632f2b2f38d2b0935e9c40c812de24c5f850d2a
Hao Kung <haok@microsoft.com>
2015-02-13 02:38:12 -0800

    AuthenticationType -> AuthenticationScheme
    Also rename IAuthTypesContext -> IDescribeSchemesContext

---
+++ src/Microsoft.AspNet.Http.Core/Security/DescribeSchemesContext.cs
@@ -0,0 +1,29 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.AspNet.Http.Security;
+using Microsoft.AspNet.Http.Interfaces.Security;
+
+namespace Microsoft.AspNet.Http.Core.Security
+{
+    public class DescribeSchemesContext : IDescribeSchemesContext
+    {
+        private List<AuthenticationDescription> _results;
+
+        public DescribeSchemesContext()
+        {
+            _results = new List<AuthenticationDescription>();
+        }
+
+        public IEnumerable<AuthenticationDescription> Results
+        {
+            get { return _results; }
+        }
+                
+        public void Accept(IDictionary<string, object> description)
+        {
+            _results.Add(new AuthenticationDescription(description));
+        }
+    }
+}
